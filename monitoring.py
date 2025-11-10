9f664bdff144d312a0ac67c6040ca288ec8971da
Nancimento <revandoaruperes6@gmail.com>
2025-11-10 20:30:19 +0800

    script automated

--- monitoring.py
+++ monitoring.py
@@ -153,7 +153,7 @@ def main():
             send_whatsapp_notification(final_message)
 
     if not alert_triggered:
-        print("Sistem aman. Tidak ada IP yang melampaui ambang batas.")
+        print("Sistem aman jaya. Tidak ada IP yang melampaui ambang batas.")
 
 if __name__ == "__main__":
     main()
